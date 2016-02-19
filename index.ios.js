8e2cb9a139ed202582e7feb093acd2950c087bd3
Dan Andersen <dandersen2@gmail.com>
2016-02-19 11:36:28 -0500

    Set up initial navigator, incomplete

--- index.ios.js
+++ index.ios.js
@@ -6,6 +6,8 @@
 import React, {
   AppRegistry,
   Component,
+  Image,
+  ListView,
   StyleSheet,
   Text,
   TouchableHighlight,
@@ -46,7 +48,7 @@ BackgroundGeolocation.configure({
   BackgroundGeolocation.on('error', (error) => {
     var type = error.type;
     var code = error.code;
-    alert(type + " Error: " + code);
+    console.log(type + " Error: " + code);
   });
 
   // This handler fires when movement states changes (stationary->moving; moving->stationary)
@@ -65,9 +67,118 @@ BackgroundGeolocation.configure({
     BackgroundGeolocation.getCurrentPosition({timeout: 30}, (location) => {
       console.log('- [js] BackgroundGeolocation received current position: ', JSON.stringify(location));
     }, function(error) {
-      alert("Location error: " + error);
+      console.log("Location error: " + error);
     });
   });
 
+var events = [
+{ name:'Dentist Appointment', startTime: '11:00AM', location: '43 Willow St.' },
+{ name:'Work', startTime: '9:00AM', location: '48 Wall St.'},
+{ name:'Meet John for drinks', startTime: '7:00PM', location: '32 Main St.' },
+];
+
+class GetMoving extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(events),
+      loaded: true,
+    });
+  }
+
+  render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderEvent}
+        style={styles.listView}/>
+    );
+  }
+
+  renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading Events...
+        </Text>
+      </View>
+    );
+  }
+
+  renderEvent(event) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri:"https://lh5.ggpht.com/-oMUKnGIr7IUeAQaNoaY0VLJ5N_6lEZC7sSOS9tRm7W72ICH8I1-DVo-ovaODRMLlU0=w300" }}
+          // "https://upload.wikimedia.org/wikipedia/commons/4/47/Comic_image_missing.png"
+          style={styles.thumbnail}/>
+        <View style={styles.rightContainer}>
+          <Text style={styles.name}>{event.name}</Text>
+          <Text style={styles.time}>Time: {event.startTime}</Text>
+          <Text style={styles.location}>Location: {event.location}</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    // borderWidth: 0.5,
+    // borderColor: '#d6d7da',
+  },
+  rightContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+  },
+  name: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  time: {
+    textAlign: 'center',
+  },
+  location: {
+    textAlign: 'center',
+  },
+  thumbnail: {
+    margin: 5,
+    width: 60,
+    height: 60,
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
+  },
+});
+
 
 AppRegistry.registerComponent('DiaryList', () => DiaryList);
+// AppRegistry.registerComponent('GetMoving', () => GetMoving);
+
+
