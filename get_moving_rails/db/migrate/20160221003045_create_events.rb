a8b0d996f9a76c5a1dbb282ba487eaf17a236f74
Mira Scarvalone <email": null>
2016-02-21 14:01:43 -0500

    Successfully post record to server with fake data

--- get_moving_rails/db/migrate/20160221003045_create_events.rb
+++ get_moving_rails/db/migrate/20160221003045_create_events.rb
@@ -6,6 +6,8 @@ class CreateEvents < ActiveRecord::Migration
       t.string :user_email, null: false
       t.datetime :start_time, null: false
       t.datetime :departure_time, null: false
+      t.boolean  :is_latest, default: true
+      t.boolean  :has_notified, default: false
 
       t.timestamps null: false
     end
