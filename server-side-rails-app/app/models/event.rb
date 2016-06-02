f5c34a6a04ca3a336d8eb02c9f001b8823aeba2c
Dan Andersen <dandersen2@gmail.com>
2016-06-02 14:04:41 -0400

    Place react native app and rails server-side app into folders

---
+++ server-side-rails-app/app/models/event.rb
@@ -0,0 +1,17 @@
+class Event < ActiveRecord::Base
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :user_email, presence: true
+  validates :start_time, presence: true
+  validates :device_token, presence: true
+
+  before_save :set_new_latest
+
+  def set_new_latest
+    prev_notified = Event.where(user_email: user_email, google_event_id: self.google_event_id).where(has_notified: true).exists?
+    if prev_notified
+      errors.add(:base, 'Event already_notified')
+    end
+  end
+
+end
